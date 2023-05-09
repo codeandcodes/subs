54c7ebcad1a757e1d60685a862b60309a412eead
eileenho <eileenho25@gmail.com>
2023-05-09 18:05:25 +0800

    add facebook oauth login

--- react_native_app/MyApp/App.tsx
+++ react_native_app/MyApp/App.tsx
@@ -1,9 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Button, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { fetchBackendData, fetchLocation } from './api';
+import auth from '@react-native-firebase/auth';
+import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
 
 const App = (): JSX.Element => {
   const [backendData, setBackendData] = useState('');
+
+  const signInWithFacebook = async () => {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
+  }
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
@@ -22,6 +46,7 @@ const App = (): JSX.Element => {
       <TouchableOpacity style={styles.button} onPress={fetchLocation}>
             <Text style={styles.buttonText}>Get Location</Text>
       </TouchableOpacity>
+      <Button title="Sign in with Facebook" onPress={signInWithFacebook} />
     </View>
   );
 };
---
+++ react_native_app/MyApp/ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MyApp
+//
+//  Created by EILEEN HO on 5/9/23.
+//
+
+import Foundation
